34df6efed02d91527be611b2334fa7a8ee69cad4
Isabella Tang <Isabellatang3425@gmail.com>
2025-06-08 14:25:06 -0400

    add comments

--- AIService/main.py
+++ AIService/main.py
@@ -15,8 +15,10 @@ from langgraph.graph import START, StateGraph
 from typing_extensions import List, TypedDict
 from pydantic import BaseModel
 
+# Load environment variables
 load_dotenv()
 
+# Ensure required environment variables are set
 if not os.environ.get("GOOGLE_API_KEY"):
     print("Please set the GOOGLE_API_KEY environment variable.")
 
@@ -28,23 +30,23 @@ if not os.environ.get("PINECONE_INDEX_NAME"):
     print("Please set the PINECONE_INDEX_NAME environment variable.")
 index_name = os.environ.get("PINECONE_INDEX_NAME")
 
+# Initialize llm models and vector store
 llm = init_chat_model("gemini-2.0-flash", model_provider="google_genai")
 embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
 
+# Initialize Pinecone vector store
 pc = Pinecone(api_key=pc_api_key)
 index = pc.Index(index_name)
-
 vector_store = PineconeVectorStore(embedding=embeddings, index=index)
 
+# Initialize FastAPI application
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Welcome to the AI Service!"}
 
-class UserMessage(BaseModel):
-    question: str
-
+# Scrape web page, only load specific tags: h1, h2, h3, p
 loader = WebBaseLoader(
     web_paths=("https://www.ontario.ca/document/your-guide-employment-standards-act-0",),
     bs_kwargs=dict(
@@ -59,6 +61,7 @@ assert len(docs) == 1
 print(f"Total characters: {len(docs[0].page_content)}")
 print(docs[0].page_content[:500])
 
+# Split document into smaller chunks
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, # chunk size (characters)
                                             chunk_overlap=200, # chunk overlap (characters)
                                                 add_start_index=True,  # track index in original document
@@ -69,26 +72,24 @@ print(f"Split blog post into {len(all_splits)} sub-documents.")
 # Index chunks
 _ = vector_store.add_documents(documents=all_splits)
 
-# Define prompt for question-answering
-# N.B. for non-US LangSmith endpoints, you may need to specify
-# api_url="https://api.smith.langchain.com" in hub.pull.
+# Load prompt template from LangChain Hub
 prompt = hub.pull("rlm/rag-prompt", 
                 api_url="https://api.smith.langchain.com")
 
 
-# Define state for application
+# Define what data is input and output of the application
 class State(TypedDict):
     question: str
     context: List[Document]
     answer: str
 
 
-# Define application steps
+# find chunks in vector store that are relevant to the question
 def retrieve(state: State):
     retrieved_docs = vector_store.similarity_search(state["question"])
     return {"context": retrieved_docs}
 
-
+# Put question and the retrieved context into the prompt, and generate an answer using the LLM
 def generate(state: State):
     docs_content = "\n\n".join(doc.page_content for doc in state["context"])
     messages = prompt.invoke({"question": state["question"], "context": docs_content})
@@ -96,11 +97,15 @@ def generate(state: State):
     return {"answer": response.content}
 
 
-# Compile application and test
+# Build the state graph so that POST /responses can invoke the graph
+# with the user question, and return the answer
 graph_builder = StateGraph(State).add_sequence([retrieve, generate])
 graph_builder.add_edge(START, "retrieve")
 graph = graph_builder.compile()
     
+# param class for user input in POST /responses
+class UserMessage(BaseModel):
+    question: str
 
 @app.post("/responses")
 def get_response(userMessage: UserMessage):
