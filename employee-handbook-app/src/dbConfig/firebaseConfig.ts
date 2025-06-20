8721dbf2cf1824ef790c129b4acb6ce0f9709e08
Mamoon Akhtar <mamoon.akhtar97@gmail.com>
2025-06-19 23:34:25 -0400

    Updated gitignore * Added axios_config file

--- employee-handbook-app/src/dbConfig/firebaseConfig.ts
+++ employee-handbook-app/src/dbConfig/firebaseConfig.ts
@@ -1,13 +1,12 @@
-import { getAuth } from "firebase/auth";
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./employee-app-b6a34-firebase-adminsdk-fbsvc-20ac186eaa");
+var serviceAccount = require("./employee-configs.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
 var db = admin.firestore();
-const auth = getAuth(app);
+const auth = admin.auth();
 export { db, auth };
 
