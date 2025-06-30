ec5fd30a4f1ac735d97403070160b4d9131e72ba
hussaino03 <m.hussainomer03@gmail.com>
2025-06-30 13:42:30 -0400

    simplify testing

--- employee-handbook-app/jest.config.js
+++ employee-handbook-app/jest.config.js
@@ -1,11 +1,9 @@
 const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files
   dir: './',
 })
 
-// Add any custom config to be passed to Jest
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'node',
@@ -21,5 +19,4 @@ const customJestConfig = {
   modulePathIgnorePatterns: ['<rootDir>/node_modules/'],
 }
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig) 
--- employee-handbook-app/jest.setup.js
+++ employee-handbook-app/jest.setup.js
@@ -1,5 +1,3 @@
-// Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
 
-// Mock environment variables
 process.env.NODE_ENV = 'test' 
