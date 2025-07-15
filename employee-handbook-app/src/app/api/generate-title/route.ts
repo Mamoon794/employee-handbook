9f31415b420a67b15832479e3c79e69d43f94a56
Mamoon Akhtar <mamoon.akhtar97@gmail.com>
2025-07-15 00:50:49 -0400

    Fixed endpoint calls

--- employee-handbook-app/src/app/api/generate-title/route.ts
+++ employee-handbook-app/src/app/api/generate-title/route.ts
@@ -1,11 +1,12 @@
 import { NextResponse } from 'next/server';
+import { updateChatTitle } from '@/models/dbOperations';
 
 
 
 export async function POST(req: Request) {
   try {
     const { message, chatId, userId } = await req.json();
-
+    console.log("URL", process.env.AI_SERVICE_URL);
     // calling fastAPI endpoint
     const fastApiResponse = await fetch(
       `${process.env.AI_SERVICE_URL}/generate-title`, 
@@ -21,18 +22,13 @@ export async function POST(req: Request) {
     const { title } = await fastApiResponse.json();
 
     // updating title
-    const updateResponse = await fetch('/api/update-title', { 
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ chatId, title }),
-    });
+    await updateChatTitle(chatId, title);
 
-    if (!updateResponse.ok) throw new Error('Failed to save title');
     
     return NextResponse.json({ title });
   } catch (error) {
     return NextResponse.json(
-      { title: "New Chat" }, 
+      { title: "New Chat", error: 'Failed to save title' }, 
       { status: 200 }
     );
   }
