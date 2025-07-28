8d6a1ee6e9eacdf4a0f81be3fd61ac9ac650ff0b
Mamoon Akhtar <mamoon.akhtar97@gmail.com>
2025-07-28 15:09:35 -0400

    Fixed the file uploading * Added a loading icon while files are being uploaded * Showing the files uploaded

--- employee-handbook-app/src/app/DashBoard/page.tsx
+++ employee-handbook-app/src/app/DashBoard/page.tsx
@@ -3,25 +3,108 @@
 import { useRouter } from 'next/navigation';
 import { useUser, UserButton } from "@clerk/nextjs"; 
 import axiosInstance from '../axios_config';
+import { useState, useEffect } from 'react';
+import { CircularProgress } from '@mui/material';
+
+type pdfFile = {
+  name: string;
+  type: string;
+  url: string;
+}
+
+type CustProps = {
+  files: pdfFile[];
+};
+
+const FilePreview: React.FC<CustProps> = ({ files }) => {
+  const handleOpen = (file: pdfFile) => {
+    window.open(file.url, "_blank", "noopener,noreferrer");
+  };
+
+  return (
+    <div className="max-h-64 overflow-y-auto w-full space-y-2 mb-3">
+    {files.map((file, index) => (
+      <div key={index}
+        onClick={() => handleOpen(file)}
+        className="cursor-pointer p-4 border rounded hover:bg-gray-100"
+      >
+        <p className="text-blue-600">{file.name}</p>
+      </div>
+    ))}
+    </div>
+  );
+};
 
 export default function Dashboard() {
+
+
   const router = useRouter();
   const { user } = useUser();
+  const [savedFiles, setSavedFiles] = useState<pdfFile[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
   const firstName = user?.firstName || "there"; 
+
+
+  useEffect(() => {
+    async function fetchCompanyDocs() {
+      const companyId = localStorage.getItem('companyId');
+      const companyDocs = await axiosInstance.get(`/api/company/docs/${companyId}`);
+      let get_files : pdfFile[]  = []
+      for (const doc of companyDocs.data.companyDocs) {
+        get_files.push({
+          name: doc.fileName,
+          type: 'application/pdf',
+          url: doc.fileUrl,
+        });
+      }
+      setSavedFiles(get_files);
+    }
+    if (localStorage.getItem('companyId')) {
+      fetchCompanyDocs();
+    }
+  }, []);
 
   async function uploadDocuments(event: React.ChangeEvent<HTMLInputElement>) {
     const files = event.target.files || [];
-    for (let i = 0; i < files.length; i++) {
-      const formData = new FormData();
-      formData.append('file', files[i]);
-      formData.append('bucketName', 'employee-handbook-app');
+    setIsLoading(true);
+    try{
+      for (let i = 0; i < files.length; i++) {
+        const formData = new FormData();
+        formData.append('file', files[i]);
+        formData.append('bucketName', 'employee-handbook-app');
 
-      await axiosInstance.post('/api/s3/upload', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
+        const urlResponse = await axiosInstance.post('/api/s3/upload', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+
+        let url = urlResponse.data.fileUrl;
+        savedFiles.push({
+          name: files[i].name,
+          type: files[i].type,
+          url: url
+        });
+
+        const documentData = {
+          fileUrl: url,
+          fileName: files[i].name,
+          uploadDate: new Date(),
+          isPublic: false,
+        };
+
+        await axiosInstance.put('/api/company/docs',{
+          companyId: localStorage.getItem('companyId'),
+          documents: [documentData],
+        });
+      }
+      setSavedFiles([...savedFiles]);
+    } catch (error) {
+      console.error("Error uploading documents:", error);
+      alert("Failed to upload documents. Please try again.");
     }
+    setIsLoading(false);
+
   }
 
   return (
@@ -47,8 +130,16 @@ export default function Dashboard() {
       <main className="flex-1 flex flex-col md:flex-row items-center justify-center gap-24 px-8 py-16 w-full max-w-7xl mx-auto">
         <div className="flex-1 flex flex-col items-center justify-center">
           <h2 className="text-4xl font-extrabold text-blue-800 mb-8 text-center">Welcome, {firstName}!</h2>
-          <p className="text-lg text-black font-bold mb-12 text-center">It seems there are currently no files uploaded.</p>
-          <label className="bg-[#294494] text-white font-extrabold px-12 py-5 rounded-xl text-xl hover:bg-blue-900 transition-colors shadow-md cursor-pointer">
+           {savedFiles.length > 0 ? (
+            <p className="text-lg text-black font-bold mb-12 text-center">You have {savedFiles.length} files uploaded:</p>
+          ) : (
+            <p className="text-lg text-black font-bold mb-12 text-center">It seems there are currently no files uploaded.</p>
+          )}
+          <FilePreview files={savedFiles} />
+          {isLoading ? (
+            <CircularProgress />
+          )
+          : (<label className="bg-[#294494] text-white font-extrabold px-12 py-5 rounded-xl text-xl hover:bg-blue-900 transition-colors shadow-md cursor-pointer">
             Upload Documents
             <input
               type="file"
@@ -58,7 +149,7 @@ export default function Dashboard() {
               onChange={uploadDocuments}
               className="hidden"
             />
-          </label>
+          </label>)}
         </div>
         {/* Employee Management Card */}
         <div className="w-full max-w-sm bg-[#f5f7fb] rounded-xl shadow-lg flex flex-col items-center py-12 px-8">
