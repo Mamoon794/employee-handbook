901777aae2d3052d6647e347c03f9b2856b7bfed
Uzima Malik <uzima.malik@mail.utoronto.ca>
2025-08-05 19:30:17 -0400

    lint

--- employee-handbook-app/components/free-trial-popup.tsx
+++ employee-handbook-app/components/free-trial-popup.tsx
@@ -1,6 +1,4 @@
 import { Dispatch, SetStateAction } from "react";
-import { useRouter } from "next/navigation";
-import { useClerk } from "@clerk/nextjs";
 
 interface FreeTrialModalProps {
   trialEndsAt: string;
